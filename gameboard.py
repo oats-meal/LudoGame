c2a44888142401271cd646f31aa00cd9d906b32a
oats-meal <26823156+oats-meal@users.noreply.github.com>
2021-05-25 21:23:18 -0700

    Adding the Gameboard

---
+++ gameboard.py
@@ -0,0 +1,10 @@
+"""
+Game Board
+
+This is the file that will store the game board and game state
+"""
+
+class gameboard(self):
+    def __init__(self, players):
+        self.players = players
+        
